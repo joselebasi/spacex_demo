03dd4f2f6edd5599fa6424d41bd65eef97e7190f
MTTOS1 <MTTOS1@serviciosexternos.com.mx>
2024-02-05 09:43:09 -0600

    feat:add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://joselebasi.github.io',
+  base: '/spacex_demo',
   integrations: [tailwind(), preact()],
   output: 'server'
 });
